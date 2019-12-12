d137a99a1ac52ea1f37b84e6de50ca0896f9b391
ChousinRahit <2428kcr@gmail.com>
2019-12-12 08:31:09 +0530

    Updates

---
+++ precache-manifest.4ae0c52683bfcbd6dd94a3af6b716a48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29db595fa2495f67704d2529aea4f88e",
+    "url": "/Flight-Search-Engine/index.html"
+  },
+  {
+    "revision": "a9b37e7852cca0f7adcb",
+    "url": "/Flight-Search-Engine/static/css/main.40be26e1.chunk.css"
+  },
+  {
+    "revision": "ef77ae510f5b222209d5",
+    "url": "/Flight-Search-Engine/static/js/2.f4389477.chunk.js"
+  },
+  {
+    "revision": "998feb35d30362b6926f58911c61c592",
+    "url": "/Flight-Search-Engine/static/js/2.f4389477.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9b37e7852cca0f7adcb",
+    "url": "/Flight-Search-Engine/static/js/main.83990093.chunk.js"
+  },
+  {
+    "revision": "a934b1ae39adb75cdba4",
+    "url": "/Flight-Search-Engine/static/js/runtime-main.85c05580.js"
+  },
+  {
+    "revision": "326dbf06be759fa6f0da32400314d2ae",
+    "url": "/Flight-Search-Engine/static/media/air-asia.326dbf06.png"
+  },
+  {
+    "revision": "d10bbd23ed3323a8e970d3ddea4575f1",
+    "url": "/Flight-Search-Engine/static/media/air-india.d10bbd23.png"
+  },
+  {
+    "revision": "3a1d216e46d7bfbb3f0928a0695bf238",
+    "url": "/Flight-Search-Engine/static/media/indigo.3a1d216e.png"
+  },
+  {
+    "revision": "106403076a7b2b5bb28d357b223f95a5",
+    "url": "/Flight-Search-Engine/static/media/jet-airways.10640307.png"
+  },
+  {
+    "revision": "1c44c07823acbb2e05438406d8003a7f",
+    "url": "/Flight-Search-Engine/static/media/spicejet.1c44c078.png"
+  }
+]);
